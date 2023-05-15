709a9782f3aacbd094db2e753fd75ab6f80fc729
Mike Shen <michael.m.shen@gmail.com>
2023-05-15 18:56:37 +0000

    added files

--- CookieStand/settings.py
+++ CookieStand/settings.py
@@ -23,7 +23,8 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG')
 
-ALLOWED_HOSTS = tuple(env.list('ALLOWED_HOSTS'))
+# ALLOWED_HOSTS = tuple(env.list('ALLOWED_HOSTS'))
+ALLOWED_HOSTS = env('ALLOWED_HOSTS')
 
 
 # Application definition
